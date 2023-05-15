bb944ecbde1e34cae68fc928e4219727ab2a1175
VGiaroli <valegiaroli2016@gmail.com>
2023-05-15 19:12:11 -0300

    Cambio de interfaz en progreso

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// $('.button-collapse').sideNav();
+
+// $('.collapsible').collapsible();
+
+// $('select').material_select();
