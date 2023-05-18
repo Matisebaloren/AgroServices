bc6cf19faecbc9e557593ab95a87c5bc7fbf1268
Matisebaloren <mati.lorenzati@gmail.com>
2023-05-17 21:22:53 -0300

    CrearControladores

---
+++ Controllers/ConsultasController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AgroServices.Data;
+using AgroServices.Models;
+
+namespace AgroServices.Controllers;
+
+public class ConsultasController : Controller
+{
+    private readonly ILogger<ConsultasController> _logger;
+
+    private AgroServicesDbContext _contexto;
+
+    public ConsultasController(ILogger<ConsultasController> logger, AgroServicesDbContext contexto)
+    {
+        _logger = logger;
+        _contexto = contexto;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
---
+++ Controllers/ValoracionesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AgroServices.Data;
+using AgroServices.Models;
+
+namespace AgroServices.Controllers;
+
+public class ValoracionesController : Controller
+{
+    private readonly ILogger<ValoracionesController> _logger;
+
+    private AgroServicesDbContext _contexto;
+
+    public ValoracionesController(ILogger<ValoracionesController> logger, AgroServicesDbContext contexto)
+    {
+        _logger = logger;
+        _contexto = contexto;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
