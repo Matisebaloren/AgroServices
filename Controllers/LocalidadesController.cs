c9648c38c5f20b9a4259341bc83d8150979723a6
Matisebaloren <mati.lorenzati@gmail.com>
2023-05-18 18:43:07 -0300

    CrearControladores

---
+++ Controllers/LocalidadesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AgroServices.Data;
+using AgroServices.Models;
+
+namespace AgroServices.Controllers;
+
+public class LocalidadesController : Controller
+{
+    private readonly ILogger<LocalidadesController> _logger;
+
+    private AgroServicesDbContext _contexto;
+
+    public LocalidadesController(ILogger<LocalidadesController> logger, AgroServicesDbContext contexto)
+    {
+        _logger = logger;
+        _contexto = contexto;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
