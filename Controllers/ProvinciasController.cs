ea8de1a5fccb5388d5df6679eab02cb1d6a08bae
Matisebaloren <mati.lorenzati@gmail.com>
2023-05-18 19:40:25 -0300

    Crear-Provincias-1.0.0.js

--- Controllers/ProvinciasController.cs
+++ Controllers/ProvinciasController.cs
@@ -8,7 +8,6 @@ namespace AgroServices.Controllers;
 public class ProvinciasController : Controller
 {
     private readonly ILogger<ProvinciasController> _logger;
-
     private AgroServicesDbContext _contexto;
 
     public ProvinciasController(ILogger<ProvinciasController> logger, AgroServicesDbContext contexto)
@@ -22,4 +21,17 @@ public class ProvinciasController : Controller
         return View();
     }
 
+    public JsonResult BuscarProvincias(int provinciaID = 0, bool Deshabilitar = false)
+    {
+        var provincias = _contexto.Provincias.ToList();
+        
+        if (provinciaID > 0)
+        {   
+            provincias = provincias.Where(c => c.ProvinciaID == provinciaID).OrderBy(c => c.nombre).ToList();
+        }
+        //Validacion para saber si estamos deshabilitando una provincia 
+        
+        _contexto.SaveChanges();
+        return Json(provincias);
+    }
 }
