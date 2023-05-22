65b475ae6fe3e65bf1261d80b93dd478f475ebe9
Matisebaloren <mati.lorenzati@gmail.com>
2023-05-22 19:03:11 -0300

    FusionDeCambios22/5

--- Controllers/ProvinciasController.cs
+++ Controllers/ProvinciasController.cs
@@ -36,6 +36,63 @@ public class ProvinciasController : Controller
     }
 
     // crea o modifica elemento de la base de datos
+
+    public JsonResult GuardarProvincia(int provinciaID, string nombre)
+    {
+        string resultado = "Error";
+
+        
+        if (!string.IsNullOrEmpty(nombre)){
+
+               
+                        //SI ES 0 QUIERE DECIR QUE ESTA CREANDO EL ELEMENTO
+                    if(provinciaID == 0){
+                        //BUSCAMOS EN LA TABLA SI EXISTE UNA CON LA MISMO NOMBRE
+                        var provinciaOriginal = _contexto.Provincias.Where(c => c.Nombre == nombre).FirstOrDefault();
+                        if(provinciaOriginal == null){
+                        
+                            //DECLAMOS EL OBJETO DANDO EL VALOR
+                            var ProvinciaGuardar = new Provincia{
+                                Nombre = nombre
+                            };
+                            _contexto.Add(ProvinciaGuardar);
+                            _contexto.SaveChanges();
+                            resultado = "Crear";
+
+                        }
+                        else{
+                            resultado = "repetir";
+                        }
+                    }
+                    else{
+                        //BUSCAMOS EN LA TABLA SI EXISTE UNA CON LA MISMA DESCRIPCION Y DISTINTO ID DE REGISTRO AL QUE ESTAMOS EDITANDO
+                        var provinciaOriginal = _contexto.Provincias.Where(c => c.Nombre == nombre && c.ProvinciaID != provinciaID).Count();
+                        // var categoriaIguales = categoriaOriginal.Where(c => c.CategoriaID == categoriaID).Count();
+                        if(provinciaOriginal == 0){
+                            //crear variable que guarde el objeto segun el id deseado
+                            var provinciaEditar = _contexto.Provincias.Find(provinciaID);
+                            if(provinciaEditar != null){
+                                provinciaEditar.Nombre = nombre;
+                                provinciaEditar.ProvinciaID = provinciaID;
+                                _contexto.SaveChanges();
+                                 resultado = "Crear";
+                            }
+                            
+                        }
+                        else{
+                            resultado = "repetir";
+                        }
+                    }                          
+        }
+        else{
+            resultado = "faltas";
+        }
+
+        return Json(resultado);
+    }
+
+
+
     // public JsonResult GuardarProvincia(int provinciaID, string descripcion)
     // {
     //     string resultado = "error";
