1e4543209503b39ebc2c42d2472da1bf1222c264
VGiaroli <valegiaroli2016@gmail.com>
2023-06-01 18:44:32 -0300

    Cambios en estetica 1/06/23

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -9,6 +9,7 @@
 
 // $('select').material_select();
 
+//FUNCIONES DEL NAVBAR VERTICALIZADO
 const body = document.querySelector("body");
 const darkLight = document.querySelector("#darkLight");
 const sidebar = document.querySelector(".sidebar");
@@ -62,3 +63,39 @@ if (window.innerWidth < 768) {
 } else {
   sidebar.classList.remove("close");
 }
+
+//FIN DE FUNCIONES DEL NAVBAR VERTICALIZADO
+
+// CREANDO FILTRADO DE BUSQUEDA INTERNO
+
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+function buscador_interno(){
+
+
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+    for (i = 0; i < li.length; i++){
+
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+            box_search.style.display = "block";
+
+            if (inputSearch.value === ""){
+                box_search.style.display = "none";
+            }
+
+        }else{
+            li[i].style.display = "none";
+        }
+
+    }
+
+  }
+
