f52a30e473e3e8b700fc127e25dedc755883f8e3
VGiaroli <valegiaroli2016@gmail.com>
2023-06-22 18:50:20 -0300

    cambios 22/06

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -123,9 +123,61 @@ submenuItems.forEach((item, index) => {
 
 // }
 
+// const searchWrapper = document.querySelector(".search-input");
+// const inputBox = searchWrapper.querySelector(".search-input");
+// const suggBox = searchWrapper.querySelector(".autocom-box");
+// const icon = searchWrapper.querySelector(".icon");
+// let linkTag = searchWrapper.querySelector(".a");
 
+// inputBox.onkeyup = (e)=>{
+//     let userData = e.target.value;
+//     let emptyArray = [];
+//     if (userData) {
+//         webLink = "https://www.google.es/search?q=$(userData)";
+//         linkTag.setAttribute("href", webLink);
+//         linkTag.click();
+//     }
+//     emptyArray = suggestions.Filter((data)=>{
 
+//         return
+//         data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+//         });
 
+//         emptyArray = emptyArray.map((data)=>{
+//             return data ="<li>${data}</li>";
+            
+//         });
 
+//         searchWrapper.classList.add("active");
+//         showSuggestions(emptyArray);
+//         let allList = suggBox.querySelectorAll("li");
+//         for (let i=0; i< allList.length; i++){
+//             allList[i].setAttribute("onclick", "select(this)"); 
+//         // }else{
+//         //     searchWrapper.classList.remove("active");
+//         // }
 
+//     }
 
+//     function select(element) {
+//         let selectData = element.textContent;
+//         inputBox.value = selectData;
+//         icon.onclick = ()=>{
+//             webLink = "https://www.google.es/search?q=$(userData)";
+//             linkTag.setAttribute("href", webLink);
+//             linkTag.click(); 
+//         }
+//         searchWrapper.classList.remove("active")
+//     }
+
+//     function showSuggestions(list) {
+//         let listData;
+//         if (list.length) {
+//             userValue=inputBox.value;
+//             listData="<li>${userValue}</li>";
+//         }else{
+//             listData=list.join("");
+//         }
+//         suggBox.innerHTML-listData;
+        
+//     }
