e19f882a74743a4d39f766ce7831956b1197e57f
VGiaroli <valegiaroli2016@gmail.com>
2023-08-23 17:57:38 -0300

    cambios 23/8

--- wwwroot/js/dataTable.js
+++ wwwroot/js/dataTable.js
@@ -1,14 +1,15 @@
 
-  $(document).ready(function () {
-  });
-  var table = $('#example').DataTable({
-      'paging': true,
-      'lengthChange': true,
-      'searching': true,
-      'ordering': true,
-      'info': false,
-      'autoWidth': false,
-     'stateSave': true,
- });
+//   $(document).ready(function () {
+//   });
+//   var table = $('#example').DataTable({
+//       'paging': true,
+//       'lengthChange': true,
+//       'searching': true,
+//       'ordering': true,
+//       'info': false,
+//       'autoWidth': false,
+//      'stateSave': true,
+//  });
 
 // $('#example').DataTable();
+
